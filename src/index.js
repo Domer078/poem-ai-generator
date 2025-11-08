147384dbc12e31fa4718abd043981f718e9fe711
Domer078 <khomotsomabala05@gmail.com>
2025-11-08 18:40:26 +0200

    Modified my code with User Experience

--- src/index.js
+++ src/index.js
@@ -15,10 +15,14 @@ function displayPoem(event) {
   let poemInput = document.querySelector("#input-poem");
 
   let apiKey = "63340413at53cc6c6ba7a81a11oc3f05";
-  let prompt = `Generate a french poem about ${poemInput.value} `;
+  let prompt = `User instructions: Generate a French poem about ${poemInput.value} `;
   let context =
-    "You are an expert when it comes to love and romance,the poem should have one stanza that contains four lines. At the end of the poem Sign with the Shecodes AI and should contain a <strong> element";
+    "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poems");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a French poem about ${poemInput.value}</div>`;
 
   axios.get(apiUrl).then(generatePoem);
 }
